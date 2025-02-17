54093fa3d41118991720fe9f71b1837d91a57ca6
Nic Delorme <nic.delorme@nrdtech.io>
2025-02-17 07:02:44 -0600

    fix

--- terraform/main/prod/ecs_global.tf
+++ terraform/main/prod/ecs_global.tf
@@ -1,4 +1,4 @@
-resource "aws_ecs_account_setting_default" "container_insights" {
-  name  = "containerInsights"
-  value = "enabled"
-}
+# resource "aws_ecs_account_setting_default" "container_insights" {
+#   name  = "containerInsights"
+#   value = "enabled"
+# }
